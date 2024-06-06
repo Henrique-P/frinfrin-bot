9d4b519e30d7a39c758d86cc1cd456d121995c6e
Henrique-P <pedrosantana762@gmail.com>
2024-06-06 20:57:21 -0300

    Rolled back getenv() to use full module name for better visibility; Added Inline Query Handler; Reworked previous methods to work both with private messages and inline queries; Added auto switch for local/cloud exec.

--- bot.py
+++ bot.py
@@ -1,20 +1,21 @@
 import logging, requests
-from re import sub
-from telegram import Update
-from telegram.ext import CommandHandler, CallbackContext, Application, MessageHandler, filters, CallbackQueryHandler
-from os import getenv, environ
-from game import rps, rpsStart
+from re import search
+from telegram import InlineQueryResultArticle, InputTextMessageContent, Update
+from telegram.ext import CommandHandler, CallbackContext, Application, MessageHandler, filters, CallbackQueryHandler, InlineQueryHandler
+import os
+# from game import rps, rpsStart
 from dotenv import load_dotenv
+from linkEmbedding import twitter, tiktok, insta, trackerRemoval
 
-if 'TOKEN' not in environ:
+if 'TOKEN' not in os.environ:
     load_dotenv()
 
-TOKEN = getenv('TOKEN')
-WEBHOOK_TOKEN = getenv('WEBHOOK_TOKEN')
-WEBHOOK_URL = getenv('WEBHOOK_URL')
-PORT = getenv('PORT')
-KEY_PATH = getenv('KEY_PATH')
-CERT_PATH = getenv('CERT_PATH')
+TOKEN = os.getenv('TOKEN')
+WEBHOOK_TOKEN = os.getenv('WEBHOOK_TOKEN')
+WEBHOOK_URL = os.getenv('WEBHOOK_URL')
+PORT = os.getenv('PORT')
+KEY_PATH = os.getenv('KEY_PATH', None)
+CERT_PATH = os.getenv('CERT_PATH', None)
 
 trackerRegexPattern = r'&?si=[^&]*|&?igsh=[^&]*'
 
@@ -22,89 +23,77 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 logger = logging.getLogger(__name__)
-    
-async def twitterHandler(update: Update, context: CallbackContext) -> None:
-    message = update.message.text
-    if not message:
-        return
-    postLink = message.split(".com/", 1)[1]
-    postLink = postLink.split("?")[0]
-    instantViewLink = "https://i.fixupx.com/" + postLink
-    if instantViewLink == message:
-        #logging.info("¯\\_(ツ)_/¯")
-        await update.message.reply_sticker("CAACAgEAAxkBAAECSm9mXAHqyk3h8vkRx7ucxyF6qQppkAACuQIAAh9lSEfeZwF56Y_N9DUE")
-        return
-    user =  update.effective_user.full_name
-    #logging.info("FxTwitter Request by %s", user)
-    apiLink = "https://api.fxtwitter.com/" + postLink
-    statusCode = requests.get(apiLink).status_code
-    if statusCode == 404:
-        await update.message.reply_text("This post might be invalid or private.")
-    else:
-        await update.message.reply_text(instantViewLink)
 
-async def tiktokHandler(update: Update, context: CallbackContext) -> None:
-    message = update.message.text
-    if not message:
-        return
-    postLink = message.split(".com/", 1)[1]
-    postLink = postLink.split("?")[0]
-    finalLink = "https://fixuptiktok.com/" + postLink
-    if finalLink == message:
-        #logging.info("¯\\_(ツ)_/¯")
-        await update.message.reply_sticker("CAACAgEAAxkBAAECSm9mXAHqyk3h8vkRx7ucxyF6qQppkAACuQIAAh9lSEfeZwF56Y_N9DUE")
-        return
-    user =  update.effective_user.full_name
-    #logging.info("FxTikTok Request by %s", user)
-    apiLink = "https://api.fxtiktok.com/" + postLink
-    statusCode = requests.get(apiLink).status_code
-    if statusCode == 404:
-        await update.message.reply_text("This post might be invalid or private.")
-    else:
-        await update.message.reply_text(finalLink)
-        
-async def instaHandler(update: Update, context: CallbackContext) -> None:
-    message = update.message.text
-    if not message:
-        return
-    postLink = message.split(".com/reel/", 1)[1]
-    postLink = postLink.split("?")[0]
-    finalLink = "https://ddinstagram.com/reel/" + postLink
-    if finalLink == message:
-        #logging.info("¯\\_(ツ)_/¯")
-        await update.message.reply_sticker("CAACAgEAAxkBAAECSm9mXAHqyk3h8vkRx7ucxyF6qQppkAACuQIAAh9lSEfeZwF56Y_N9DUE")
-        return
-    user =  update.effective_user.full_name
-    #logging.info("Instagram Request by %s", user)
-    await update.message.reply_text(finalLink)
-    
 async def start(update: Update, context: CallbackContext) -> None:
     user =  update.effective_user.full_name
-    #logging.info("%s started the bot", user)
+    logging.info("%s started the bot", user)
     await update.message.reply_text("Hello! Send me a Twitter, TikTok or Instagram link.")
-    
-async def trackerRemovalHandler(update: Update, context: CallbackContext) -> None:
-    await update.message.reply_text("Tracker Found. Here is the clean link:")
-    cleanLink = sub(trackerRegexPattern,"", update.message.text)
-    if not cleanLink:
-        await update.message.reply_text("https://youtu.be/dQw4w9WgXcQ", disable_web_page_preview=True)
-        user =  update.effective_user.full_name
-        #logging.info("%s got RickRoll'd", user)
+        
+async def inlineMessage(update: Update, context: CallbackContext) -> None:
+    query = update.inline_query.query
+    user = update.inline_query.from_user
+    if not query:
+        return
+    if search(r'(twitter|x)\.com/.+/status/.+',query):
+        response = twitter(query)
+        logging.info("FxTwitter Request by %s", user)
+    elif search(r'tiktok\.com/@.+/video/.+',query):
+        response = tiktok(query)
+        logging.info("FxTikTok Request by %s", user)
+    elif search(r'instagram\.com/reel/.+', query):
+        response = insta(query)
+        logging.info("DDInstagram Request by %s", user)
+    elif search(trackerRegexPattern, query):
+        response = trackerRemoval(query)
+        logging.info("Generic Tracker Removal Request by %s", user)
     else:
-        await update.message.reply_text(cleanLink)
-        user =  update.effective_user.full_name
-        #logging.info("%s removed a tracker", user)
+        return
+    if query == response:
+        return
+    elif not response:
+        return
+    answer = [InlineQueryResultArticle(response,'Interactive',InputTextMessageContent(response),description='Send an Interactive View link')]
+    await update.inline_query.answer(answer)
+        
+async def privateMessage(update: Update, context: CallbackContext) -> None:
+    message = update.message.text
+    user = update.effective_user.full_name
+    if not message:
+        return
+    if search(r'(twitter|x)\.com/.+/status/.+', message):
+        response = twitter(message)
+        logging.info("FxTwitter Request by %s", user)
+    elif search(r'tiktok\.com/@.+/video/.+', message):
+        response = tiktok(message)
+        logging.info("FxTikTok Request by %s", user)
+    elif search(r'instagram\.com/reel/.+', message):
+        response = insta(message)
+        logging.info("DDInstagram Request by %s", user)
+    elif search(trackerRegexPattern, message):
+        response = trackerRemoval(message)
+        logging.info("Generic Tracker Removal Request by %s", user)
+    else:
+        return
+    if message == response:
+        logging.info("¯\\_(ツ)_/¯")
+        await update.message.reply_sticker("CAACAgEAAxkBAAECSm9mXAHqyk3h8vkRx7ucxyF6qQppkAACuQIAAh9lSEfeZwF56Y_N9DUE")
+        return
+    elif not response:
+        logging.info("%s got RickRoll'd", user)
+        await update.message.reply_text("Here's your link:\nhttps://youtu.be/dQw4w9WgXcQ", disable_web_page_preview=True)
+        return
+    await update.message.reply_text("Here's your link:")
+    await update.message.reply_text(response)
 
 def main() -> None:
     application = Application.builder().token(TOKEN).build()
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(MessageHandler(filters.Regex(r'(twitter|x)\.com/.+/status/.+'), twitterHandler))
-    application.add_handler(MessageHandler(filters.Regex(r'tiktok\.com/@.+/video/.+'), tiktokHandler))
-    application.add_handler(MessageHandler(filters.Regex(r'instagram\.com/reel/.+'), instaHandler))
-    application.add_handler(MessageHandler(filters.Regex(trackerRegexPattern), trackerRemovalHandler))
-    application.add_handler(CommandHandler("rps", rpsStart))
-    application.add_handler(CallbackQueryHandler(rps,r'Rock|Paper|Scissors'))    
-    application.run_webhook(listen='0.0.0.0', port=PORT, secret_token=WEBHOOK_TOKEN, key=KEY_PATH, cert=CERT_PATH, webhook_url=WEBHOOK_URL+PORT)
+    application.add_handler(MessageHandler(filters.TEXT, privateMessage))
+    application.add_handler(InlineQueryHandler(inlineMessage))
+    if not KEY_PATH:
+        application.run_webhook(listen='0.0.0.0', port=PORT, secret_token=WEBHOOK_TOKEN, webhook_url=WEBHOOK_URL+PORT)
+    else:
+        application.run_webhook(listen='0.0.0.0', port=PORT, secret_token=WEBHOOK_TOKEN, key=KEY_PATH, cert=CERT_PATH, webhook_url=WEBHOOK_URL+PORT)
     
 
 if __name__ == '__main__':
