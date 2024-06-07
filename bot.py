b1d8212e7a6d17ba1f9ac5e68628ee11b81fbfd4
Henrique-P <pedrosantana762@gmail.com>
2024-06-07 18:03:21 -0300

    Added new logging and log handler.

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import logging
+import time
 from re import search
 from telegram import InlineQueryResultArticle, InputTextMessageContent, Update
 from telegram.ext import CommandHandler, CallbackContext, Application, MessageHandler, filters, InlineQueryHandler
@@ -16,46 +17,74 @@ PORT = os.getenv('PORT')
 KEY_PATH = os.getenv('KEY_PATH', None)
 CERT_PATH = os.getenv('CERT_PATH', None)
 
+userRegistry = {"linkX": 0, "linkInsta": 0, "linkFurAffinity": 0, "linkTikTok": 0, "linkGeneric": 0, "genericTxt": 0, "commandStart": 0, "commandLog": 0, "admin": 0, "qa": 0, "noService": 0}
+startUpTime= time.ctime()
+def printUserRegistry():
+    logString = ""
+    for key, value in userRegistry.items():        
+        logString += f'{key}: {value}\n'
+    return logString
+
+def addToRegistry(key: str, user: int, admin= 23742393):
+    if user != admin:
+        userRegistry[key] += 1
+    else:
+        userRegistry["admin"] += 1
+
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 logger = logging.getLogger(__name__)
 
 async def start(update: Update, context: CallbackContext) -> None:
-    user =  update.effective_user.full_name
-    logging.info("%s started the bot", user)
+    user =  update.effective_user.id
+    # logging.info("%s started the bot", user)
+    addToRegistry("commandStart", user)
     await update.message.reply_text("Hello! Send me a Twitter, TikTok or Instagram link.")
+    
+async def log(update: Update, context: CallbackContext) -> None:
+    user =  update.effective_user.id
+    # logging.info("%s acessed the log", user)
+    addToRegistry("commandLog", user)
+    response = printUserRegistry()
+    await update.message.reply_text(response)
+    await update.message.reply_text(f'Counting Since: {startUpTime}')
         
 async def inlineMessage(update: Update, context: CallbackContext) -> None:
     query = update.inline_query.query
-    user = update.inline_query.from_user
+    user = update.inline_query.from_user.id
     if not query:
         return
     if search(r'(twitter|x)\.com/.+/status/.+',query):
         response = twitter(query)
         thumbUrl = 'https://cdn.freelogovectors.net/wp-content/uploads/2023/07/twitter-x-logo-freelogovectors.net_.png'
         title = "X"
-        logging.info("FxTwitter Request by %s", user)
+        # logging.info("FxTwitter Request by %s", user)
+        addToRegistry("linkX", user)
     elif search(r'tiktok\.com/@.+/video/.+',query):
         response = tiktok(query)
         thumbUrl = ''
         title = "TikTok"
-        logging.info("FxTikTok Request by %s", user)
+        addToRegistry("linkTikTok", user)
+        # logging.info("FxTikTok Request by %s", user)
     elif search(r'instagram\.com/reel/.+', query):
         response = insta(query)
         thumbUrl = ''
         title = "Instagram"
-        logging.info("DDInstagram Request by %s", user)
+        addToRegistry("linkInstagram", user)
+        # logging.info("DDInstagram Request by %s", user)
     elif search(r'furaffinity\.net/view/.+', query):
         response = furAffinity(query)
         thumbUrl = 'https://logos-world.net/wp-content/uploads/2024/02/FurAffinity-Logo-500x281.png'
         title = "FurAffinity"
-        logging.info("FxFurAffinity Request by %s", user)
+        addToRegistry("linkFurAffinity", user)
+        # logging.info("FxFurAffinity Request by %s", user)
     elif search(trackerRegexPattern, query):
         response = trackerRemoval(query)
         thumbUrl = ''
         title = "Tracker Removed"
-        logging.info("Generic Tracker Removal Request by %s", user)
+        addToRegistry("genericLink", user)
+        # logging.info("Generic Tracker Removal Request by %s", user)
     else:
         return
     if query == response:
@@ -67,34 +96,40 @@ async def inlineMessage(update: Update, context: CallbackContext) -> None:
         
 async def privateMessage(update: Update, context: CallbackContext) -> None:
     message = update.message.text
-    user = update.effective_user.full_name
+    user = update.effective_user.id
     if not message:
         return
     if search(r'(twitter|x)\.com/.+/status/.+', message):
         response = twitter(message)
-        logging.info("FxTwitter Request by %s", user)
+        addToRegistry("linkX", user)
+        # logging.info("FxTwitter Request by %s", user)
     elif search(r'tiktok\.com/@.+/video/.+', message):
         response = tiktok(message)
-        logging.info("FxTikTok Request by %s", user)
+        addToRegistry("linkTikTok", user)
+        # logging.info("FxTikTok Request by %s", user)
     elif search(r'instagram\.com/reel/.+', message):
         response = insta(message)
-        logging.info("DDInstagram Request by %s", user)
+        addToRegistry("linkInstagram", user)
+        # logging.info("DDInstagram Request by %s", user)
     elif search(r'furaffinity\.net/view/.+', message):
         response = furAffinity(message)
-        logging.info("FxFurAffinity Request by %s", user)
+        addToRegistry("linkFurAffinity", user)
+        # logging.info("FxFurAffinity Request by %s", user)
     elif search(trackerRegexPattern, message):
         response = trackerRemoval(message)
-        logging.info("Generic Tracker Removal Request by %s", user)
+        addToRegistry("linkGenericLink", user)
+        # logging.info("Generic Tracker Removal Request by %s", user)
     else:
+        addToRegistry("genericTxt", user)
         await update.message.reply_text("Message not recognized. Try sending me a Instagram, Twitter, Furaffinity or TikTok link and I will embed and/or remove any trackers.")
         return
     if message == response:
-        logging.info("¯\\_(ツ)_/¯")
+        addToRegistry("noService", user)
         await update.message.reply_text("Nothing to do with this link.")
         await update.message.reply_sticker("CAACAgEAAxkBAAECSm9mXAHqyk3h8vkRx7ucxyF6qQppkAACuQIAAh9lSEfeZwF56Y_N9DUE")
         return
     elif not response:
-        logging.info("%s got RickRoll'd", user)
+        addToRegistry("qa", user)
         await update.message.reply_text("Here's your link:\nhttps://youtu.be/dQw4w9WgXcQ", disable_web_page_preview=True)
         return
     
@@ -104,6 +139,7 @@ async def privateMessage(update: Update, context: CallbackContext) -> None:
 def main() -> None:
     application = Application.builder().token(TOKEN).build()
     application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("log", log))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, privateMessage))
     application.add_handler(InlineQueryHandler(inlineMessage))
     if not KEY_PATH:
