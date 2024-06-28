e7aebb8878110966db65507af095770366b38d64
Henrique-P <pedrosantana762@gmail.com>
2024-06-27 22:56:14 -0300

    added privacy handler

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ logger = logging.getLogger(__name__)
 
 async def start(update: Update, context: CallbackContext) -> None:
     customLog.logEvent()
-    await update.message.reply_text(f"Hello, {update.effective_user.first_name}!\nSee /support for all the supported medias.\nFor use help, click here: /help")
+    await update.message.reply_text(f"Hello, {update.effective_user.first_name}!\nSee /support for all the supported medias.\nFor use help, click here: /help\nYou can also check this channel if you want to know more about this bot's development:\nhttps://t.me/FrinFrinNews")
     
 async def help(update: Update, context: CallbackContext) -> None:
     customLog.logEvent()
@@ -38,6 +38,9 @@ async def help(update: Update, context: CallbackContext) -> None:
 
 async def support(update: Update, context: CallbackContext) -> None:
     await update.message.reply_text("Currently supported platforms for embedding are: Twitter, TikTok, Instagram and Furaffinity.\nI can also remove URL trackers from Spotify and Youtube links.")
+    
+async def privacy(update: Update, context: CallbackContext) -> None:
+    await update.message.reply_text("This bot keeps no data about how you use it. No user data, links or messages are logged.\nThis bot does not have ANY association with the people responsible for the embedding services used.")
 
 async def log(update: Update, context: CallbackContext) -> None:
     response = customLog.getFormattedStatistics()
@@ -115,6 +118,7 @@ def main() -> None:
     application.add_handler(CommandHandler("help", help))
     application.add_handler(CommandHandler("support", support))
     application.add_handler(CommandHandler("log", log))
+    application.add_handler(CommandHandler("privacy", privacy))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, privateMessage))
     application.add_handler(InlineQueryHandler(inlineMessage))
     if not KEY_PATH:
