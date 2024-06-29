2c9783f73f3616a9949a1eca6bad10c846cd8e7c
Henrique-P <pedrosantana762@gmail.com>
2024-06-29 14:37:37 -0300

    Renamed file and class

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from telegram import InlineQueryResultArticle, InputTextMessageContent, Update
 from telegram.ext import CommandHandler, CallbackContext, Application, MessageHandler, filters, InlineQueryHandler
 import os
 import dotenv
-import userStats
+from botInfo import botInfo
 import embed
 
 if 'TOKEN' not in os.environ:
@@ -19,7 +19,7 @@ KEY_PATH = os.getenv('KEY_PATH', None)
 CERT_PATH = os.getenv('CERT_PATH', None)
 PING_URL = os.getenv('PING_URL', None)
 
-customLog = userStats.UsageStatistics()
+botStatus = botInfo()
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -27,32 +27,32 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 async def start(update: Update, context: CallbackContext) -> None:
-    customLog.logEvent()
+    botStatus.logEvent()
     await update.message.reply_text(f"Hello, {update.effective_user.first_name}!\nSee /support for all the supported medias.\nFor use help, click here: /help\nYou can also check this channel if you want to know more about this bot's development:\nhttps://t.me/FrinFrinNews")
     
 async def help(update: Update, context: CallbackContext) -> None:
-    customLog.logEvent()
+    botStatus.logEvent()
     await update.message.reply_text("Alrighty. Lets walk you through this scenario: You were browsing twitter and sent your friend this post:\nhttps://twitter.com/FopsHourly/status/1806281425915883661")
     await update.message.reply_text("Notice that despite the content being a video, twitter only allows you both to preview a static frame of the video. Now lets try using a service to embed the video in this link:\nhttps://fxtwitter.com/FopsHourly/status/1806281425915883661")
     await update.message.reply_text("Much better, right? Now you can try sending me a link for one of the supported social medias and I'll reply to you with the embedded link.\nFor full functionality guide or suggestions please contact @Yolfrin anytime.")
 
 async def support(update: Update, context: CallbackContext) -> None:
-    customLog.logEvent()
+    botStatus.logEvent()
     await update.message.reply_text("Currently supported platforms for embedding are: Twitter, TikTok, Instagram and Furaffinity.\nI can also remove URL trackers from Spotify and Youtube links.")
     
 async def privacy(update: Update, context: CallbackContext) -> None:
-    customLog.logEvent()
+    botStatus.logEvent()
     await update.message.reply_text("This bot keeps no data about how you use it. No user data, links or messages are logged.\nThis bot does not have ANY association with the people responsible for the embedding services used.")
 
 async def log(update: Update, context: CallbackContext) -> None:
-    response = customLog.getFormattedStatistics()
+    response = botStatus.getFormattedStatistics()
     await update.message.reply_text(response)
         
 async def inlineMessage(update: Update, context: CallbackContext) -> None:
     if not update.inline_query.query:
         return
     query = update.inline_query.query
-    customLog.logEvent()
+    botStatus.logEvent()
     if re.search(r'(twitter|x)\.com/.+/status/[0-9]+',query):
         response = embed.twitter(query)
         thumbUrl = 'https://cdn.freelogovectors.net/wp-content/uploads/2023/07/twitter-x-logo-freelogovectors.net_.png'
@@ -84,7 +84,7 @@ async def privateMessage(update: Update, context: CallbackContext) -> None:
     if not update.message.text:
         return
     message = update.message.text
-    customLog.logEvent()
+    botStatus.logEvent()
     if re.search(r'(twitter|x)\.com/.+/status/[0-9]+', message):
         response = embed.twitter(message)
     elif re.search(r'tiktok\.com/.+', message):
