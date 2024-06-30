3469cff5932e0bd8d61aa9382f13735dc83e0600
Henrique-P <pedrosantana762@gmail.com>
2024-06-30 19:16:14 -0300

    Finished refactor for remaining platforms. Commented unused regex strings.

--- embed.py
+++ embed.py
@@ -4,20 +4,23 @@ import requests
 from telegram import InlineQueryResultArticle, InputTextMessageContent, Update
 from telegram.ext import CallbackContext
 
-trackerRegexPattern = r'si=[^&]*&?|igsh=[^&]*&?'
+#trackerRegexPattern = r'si=[^&]*&?|igsh=[^&]*&?'
 
 twitterPattern = r'(twitter|x)\.com/.+/status/[0-9]+'
 twitterInlinePattern = r'.+(twitter|x)\.com/.+/status/[0-9]+'
-tiktokShortPattern = r'vm\.tiktok\.com/[^/]+|tiktok\.com/t/[^/]+'
+#tiktokShortPattern = r'vm\.tiktok\.com/[^/]+|tiktok\.com/t/[^/]+'
 tiktokFullPattern = r'tiktok\.com/@[^/]+/video/[0-9]+'
 tiktokInlinePattern = r'.+tiktok\.com/.+'
+#tiktokCompletePattern = r'tiktok\.com/@[^/]+/video/[0-9]+|vm\.tiktok\.com/[^/]+|tiktok\.com/t/[^/]+'
+tiktokCompletePattern = r'tiktok\.com/@[^/]+/video/[0-9]+tiktok\.com/t/[^/]+'
 instaPattern = r'instagram\.com/reel/.+'
 instaInlinePattern = r'.+instagram\.com/reel/.+'
 furAffinityPattern = r'furaffinity\.net/view/.+'
 furAffinityInlinePattern = r'.+furaffinity\.net/view/.+'
 
 async def twitterHandler(update: Update, context: CallbackContext):
-    decomposedLink = context.match.group().split('/')
+    url = context.match.group()
+    decomposedLink = url.split('/')
     response = twitterEmbed(decomposedLink)
     if response:
         await update.message.reply_text(response)
@@ -25,8 +28,8 @@ async def twitterHandler(update: Update, context: CallbackContext):
         await update.message.reply_text("This URL is either invalid or the content is private.")
 
 async def twitterInlineHandler(update: Update, context: CallbackContext):
-    #decomposedLink = context.match.re.search(twitterPattern).group().split('/')
-    decomposedLink = re.search(twitterPattern, update.inline_query.query).group().split('/')
+    url = re.search(twitterPattern, update.inline_query.query).group()
+    decomposedLink = url.split('/')
     response = twitterEmbed(decomposedLink)
     if response:
         thumbUrl = 'https://cdn.freelogovectors.net/wp-content/uploads/2023/07/twitter-x-logo-freelogovectors.net_.png'
@@ -49,10 +52,12 @@ def twitterEmbed(link:str):
     if isPostOK:
         return composed
 
-async def tiktokFullHandler(update: Update, context: CallbackContext):
-    response = requests.get('https://' + context.match.group(), timeout=1)
-    postLink = re.search(tiktokFullPattern, response.url).group()
-    decomposedLink = context.match.group().split('/')
+async def tiktokHandler(update: Update, context: CallbackContext):
+    url = context.match.group()
+    response = requests.get('https://www.' + url , timeout=1)
+    if len(response.history):
+        url = re.search(tiktokFullPattern, response.url).group()
+    decomposedLink = url.split('/')
     domain = decomposedLink[0]
     userHandle = decomposedLink[1]
     postType = decomposedLink[2]
@@ -61,23 +66,28 @@ async def tiktokFullHandler(update: Update, context: CallbackContext):
     finalLink = f"{prefix}{domain}/{userHandle}/{postType}/{postId}"
     await update.message.reply_text(finalLink)
 
-async def tiktokShortHandler(update: Update, context: CallbackContext):
-    response = requests.get('https://' + context.match.group(), timeout=1)
-    postLink = re.search(tiktokFullPattern, response.url).group()
-    if postLink:
-        decomposedLink = postLink.split('/')
-        domain = decomposedLink[0]
-        userHandle = decomposedLink[1]
-        postType = decomposedLink[2]
-        postId = decomposedLink[3]
-        prefix = 'fixup'
-        finalLink = f"{prefix}{domain}/{userHandle}/{postType}/{postId}"
-        await update.message.reply_text(finalLink)
-    else:
-        await update.message.reply_text("This URL is either invalid or the content is private.")
+async def tiktokInlineHandler(update: Update, context: CallbackContext):
+    matches = re.search(tiktokCompletePattern, update.inline_query.query)
+    url = matches.group()
+    response = requests.get('https://www.' + url , timeout=1)
+    if len(response.history):
+        url = re.search(tiktokFullPattern, response.url).group()
+    decomposedLink = url.split('/')
+    domain = decomposedLink[0]
+    userHandle = decomposedLink[1]
+    postType = decomposedLink[2]
+    postId = decomposedLink[3]
+    prefix = 'fixup'
+    finalLink = f"{prefix}{domain}/{userHandle}/{postType}/{postId}"
+    thumbUrl = 'https://cdn.freelogovectors.net/wp-content/uploads/2023/07/twitter-x-logo-freelogovectors.net_.png'
+    title = "Post Found"
+    description = 'Send this TikTok link!'
+    answer = [InlineQueryResultArticle(str(uuid.uuid4()), title, InputTextMessageContent(finalLink), thumbnail_url=thumbUrl, description=description)]
+    await update.inline_query.answer(answer)
 
 async def instaHandler(update: Update, context: CallbackContext):
-    decomposedLink = context.match.group().split('/')
+    url = context.match.group()
+    decomposedLink = url.split('/')
     domain = decomposedLink[0]
     postType = decomposedLink[1]
     postId = decomposedLink[2]
@@ -86,11 +96,41 @@ async def instaHandler(update: Update, context: CallbackContext):
     finalLink = f"{prefix}{domain}/{postType}/{postId}"
     await update.message.reply_text(finalLink)
 
+async def instaInlineHandler(update: Update, context: CallbackContext):
+    url = re.search(instaPattern, update.inline_query.query).group()
+    decomposedLink = url.split('/')
+    domain = decomposedLink[0]
+    postType = decomposedLink[1]
+    postId = decomposedLink[2]
+    #trackers = decomposedLink[3]
+    prefix = 'dd'
+    finalLink = f"{prefix}{domain}/{postType}/{postId}"
+    thumbUrl = 'https://cdn.freelogovectors.net/wp-content/uploads/2023/07/twitter-x-logo-freelogovectors.net_.png'
+    title = "Post Found"
+    description = 'Send this Instagram link!'
+    answer = [InlineQueryResultArticle(str(uuid.uuid4()), title, InputTextMessageContent(finalLink), thumbnail_url=thumbUrl, description=description)]
+    await update.inline_query.answer(answer)
+
 async def furAffinityHandler(update: Update, context: CallbackContext):
-    decomposedLink = context.match.group().split('/')
+    url = context.match.group()
+    decomposedLink = url.split('/')
     domain = decomposedLink[0]
     postType = decomposedLink[1]
     postId = decomposedLink[2]
     prefix = 'fx'
     finalLink = f"{prefix}{domain}/{postType}/{postId}"
     await update.message.reply_text(finalLink)
+
+async def furAffinityInlineHandler(update: Update, context: CallbackContext):
+    url = re.search(furAffinityPattern, update.inline_query.query).group()
+    decomposedLink = url.split('/')
+    domain = decomposedLink[0]
+    postType = decomposedLink[1]
+    postId = decomposedLink[2]
+    prefix = 'fx'
+    finalLink = f"{prefix}{domain}/{postType}/{postId}"
+    thumbUrl = 'https://cdn.freelogovectors.net/wp-content/uploads/2023/07/twitter-x-logo-freelogovectors.net_.png'
+    title = "Post Found"
+    description = 'Send this Instagram link!'
+    answer = [InlineQueryResultArticle(str(uuid.uuid4()), title, InputTextMessageContent(finalLink), thumbnail_url=thumbUrl, description=description)]
+    await update.inline_query.answer(answer)
