deb1ad16cb4c14473ed10412438ffc3edb55774a
Henrique-P <pedrosantana762@gmail.com>
2024-08-03 17:10:04 -0300

    Implemented method linkValidation

--- embed.py
+++ embed.py
@@ -6,7 +6,7 @@ trackerRegexPattern = r'si=[^&]*&?|igsh=[^&]*&?'
 def twitter(originalLink: str):
     postLink = re.search(r'[^/]+/status/[0-9]+', originalLink).group()
     apiLink = "https://api.fxtwitter.com/" + postLink
-    statusCode = requests.get(apiLink, timeout=1).status_code
+    statusCode = validateLink(apiLink)
     if statusCode != 200:
         return -1
     else:
@@ -37,3 +37,14 @@ def trackerRemoval(originalLink: str):
     cleanLink = re.sub(trackerRegexPattern,"", originalLink)
     cleanLink = re.sub(r'\?$','', cleanLink)
     return cleanLink
+
+def validateLink(link: str):
+    try:
+        statusCode = requests.get(link, timeout=1).status_code
+    except requests.exceptions.Timeout:
+        try:
+            statusCode = requests.get(link, timeout=1).status_code
+        except requests.exceptions.Timeout:
+            return False
+        return statusCode
+    return statusCode
