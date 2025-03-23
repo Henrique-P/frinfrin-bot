004cecc27d7f996b8bb562222d5e7f870d9dd147
Henrique-P <pedrosantana762@gmail.com>
2025-03-23 18:10:11 -0300

    Bugfix in channel posts

--- embed.py
+++ embed.py
@@ -6,19 +6,15 @@ from telegram import InlineQueryResultArticle, InputTextMessageContent, Update
 #trackerRegexPattern = r'si=[^&]*&?|igsh=[^&]*&?'
 
 async def twitter(update: Update, context: CallbackContext):
-    if update.message.text:
-        postId = update.message.text.split(".com/", 1)[1]
-        await update.message.reply_text("https://fixupx.com/" + postId)
-        return
-    elif update.channel_post.text:
-        postId = update.channel_post.text.split(".com/", 1)[1]
+    if update.effective_message:
+        postId = update.effective_message.text.split(".com/", 1)[1]
         await update.effective_sender.send_message("https://fixupx.com/" + postId)
         return
-    elif update.inline_query.query:
-        postId = update.inline_query.query.split(".com/", 1)[1]
-        answer = [InlineQueryResultArticle(str(uuid4()), 'X', InputTextMessageContent("https://fixupx.com/" + postId), thumbnail_url='https://cdn.freelogovectors.net/wp-content/uploads/2023/07/twitter-x-logo-freelogovectors.net_.png')]
-        await update.inline_query.answer(answer)
-        return
+    # elif update.inline_query.query:
+    #     postId = update.inline_query.query.split(".com/", 1)[1]
+    #     answer = [InlineQueryResultArticle(str(uuid4()), 'X', InputTextMessageContent("https://fixupx.com/" + postId), thumbnail_url='https://cdn.freelogovectors.net/wp-content/uploads/2023/07/twitter-x-logo-freelogovectors.net_.png')]
+    #     await update.inline_query.answer(answer)
+    #     return
 
 # def tiktok(originalLink: str):
 #     if re.search(r'vm\.tiktok\.com/.+|tiktok\.com/t/.+',originalLink):
